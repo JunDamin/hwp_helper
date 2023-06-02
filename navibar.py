143e3b8565b8ef9eb6bfbf8b92d12282fd0473c7
JunDamin <freedomgod@gmail.com>
2023-06-02 13:19:40 +0900

    change navibar layout

--- navibar.py
+++ navibar.py
@@ -16,20 +16,20 @@ class NaviBar(ctk.CTkFrame):
 
 
         hwp_btn = ctk.CTkButton(
-            self, text="한글보이기", command=context["helper"].check_hwp, width=70,
+            self, text="한글보이기", command=context["helper"].check_hwp, width=80,
         )
         hwp_btn.grid(row=0, column=0, padx=2, pady=2)
         ToolTip(hwp_btn, text="연결되어 있는 한글을 보입니다. 연결된 한글이 없으면 새로 엽니다.")
 
 
         fullscreen_btn = ctk.CTkButton(
-            self, text="전체화면", command=context["helper"].set_fullscreen, width=70,
+            self, text="전체화면", command=context["helper"].set_fullscreen, width=80,
         )
         fullscreen_btn.grid(row=0, column=1, padx=2, pady=2)
         ToolTip(fullscreen_btn, text="현재 앱 폭에 맞춰 전체화면으로 설정합니다.")
 
         halfscreen_btn = ctk.CTkButton(
-            self, text="오른쪽화면", command=context["helper"].set_halfscreen, width=70,
+            self, text="오른쪽화면", command=context["helper"].set_halfscreen, width=80,
         )
         halfscreen_btn.grid(row=0, column=2, padx=2, pady=2)
         ToolTip(halfscreen_btn, text="현재 앱 폭에 맞춰 화면의 절반으로 설정합니다.")
@@ -39,14 +39,14 @@ class NaviBar(ctk.CTkFrame):
         add_template_btn = ctk.CTkButton(
             self, text="선택영역 탬플릿 추가", command=self.add_template
         )
-        add_template_btn.grid(row=1, column=0, columnspan=2, padx=2, pady=2)
+        add_template_btn.grid(row=1, column=0, columnspan=2, padx=2, pady=2, sticky='nesw')
         ToolTip(add_template_btn, "한글에서 현재 선택중인 영역을 탬플릿으로 추가합니다.")
 
 
         update_btn = ctk.CTkButton(
             self, text="탬플릿 관리", command=self.update_templates, width=80
         )
-        update_btn.grid(row=1, column=2,)
+        update_btn.grid(row=1, column=2,pady=2, padx=2)
         ToolTip(update_btn, text="탬플릿 이름을 수정하거나 삭제, 또는 templates 폴더에 있는 내용으로 전체를 업데이트 합니다.")
 
 
@@ -144,7 +144,7 @@ if __name__=="__main__":
     
     from types import SimpleNamespace
     # create mockup
-    d = {'set_fullscreen': 1, 'b': 2}
+    d = {'set_fullscreen': 1, "set_halfscreen": 1, 'check_hwp': 2}
     ns = SimpleNamespace(**d)
 
     root = ctk.CTk()
