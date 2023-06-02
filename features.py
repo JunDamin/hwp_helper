0795ef1bbadd6434037756de8c1789e2eca9e153
JunDamin <freedomgod@gmail.com>
2023-06-02 14:47:04 +0900

    split callback functions

--- features.py
+++ features.py
@@ -1,9 +1,9 @@
 import customtkinter as ctk
 import tkinter as tk
 from functions import (
-    back_to_app,
     get_path,
 )
+from callback import set_cell_border, set_header_style, set_para_indent, decrease_line_spacing, increase_line_spacing, break_page, break_section, setup_koica_page, setup_normal_page, super_script, insert_memo, delete_memo, insert_endnote, insert_footnote
 from components import ToolTip, FontStyleBtns, GridFrame
 
 
@@ -142,68 +142,6 @@ class HwpFeatureFrame(ctk.CTkScrollableFrame):
         for parent, name, command, text, gif in table_contents:
             btn = set_feature_btn(parent, name, command, text, gif)
             parent.add_widget(btn, pady=2, padx=2)
-
-@back_to_app
-def set_cell_border(app):
-    return app.set_cell_border(
-        top=1, bottom=1, right=0, left=0, top_width=0.4, bottom_width=0.4
-    )
-
-@back_to_app
-def set_header_style(app):
-    app.set_cell_color(bg_color=(250, 243, 219))
-    app.set_cell_border(bottom=8, bottom_width=0.5)
-    return None
-
-@back_to_app
-def set_para_indent(app):
-    return app.actions.ParagraphShapeIndentAtCaret().run()
-
-@back_to_app
-def break_section(app):
-    return app.actions.BreakSection().run()
-
-@back_to_app
-def super_script(app):
-    return app.actions.CharShapeSuperscript().run()
-
-@back_to_app
-def break_page(app):
-    return app.actions.BreakPage().run()
-
-@back_to_app
-def insert_endnote(app):
-    return app.actions.InsertEndnote().run()
-
-@back_to_app
-def insert_footnote(app):
-    return app.actions.InsertFootnote().run()
-
-@back_to_app
-def insert_memo(app):
-    return app.actions.InsertFieldMemo().run()
-
-@back_to_app
-def delete_memo(app):
-    return app.actions.DeleteFieldMemo().run()
-
-@back_to_app
-def increase_line_spacing(app):
-    return app.actions.ParagraphShapeIncreaseLineSpacing().run()
-
-@back_to_app
-def decrease_line_spacing(app):
-    return app.actions.ParagraphShapeDecreaseLineSpacing().run()
-
-@back_to_app
-def setup_koica_page(app):
-    return app.setup_page(
-        top=30, bottom=15, left=20, right=15, header=0, footer=15, gutter=0
-    )
-
-@back_to_app
-def setup_normal_page(app):
-    return app.setup_page()
 
 
 # set a function for btn
