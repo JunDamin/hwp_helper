5ac6c473e3990ca91374d8f4c3dddea38d14755f
JunDamin <freedomgod@gmail.com>
2023-12-12 11:18:37 +0900

    reactoring main page

--- core.py
+++ core.py
@@ -1,16 +1,10 @@
 import customtkinter as ctk
 import tkinter as tk
-from PIL import Image
+from PIL import Image, ImageTk
 import yaml
 from functions import (
-    get_screen_size,
-    set_window_position,
-    get_window_position,
-    get_ratio,
-    get_path,
-    check_app,
-    set_forewindow,
-    show_window,
+    get_screen_size, set_window_position, get_window_position,
+    get_ratio, get_path, check_app, set_forewindow, show_window,
 )
 from features import HwpFeatureFrame
 from templates import CategoryFrame
@@ -18,83 +12,103 @@ from navibar import NaviBar
 import win32gui as wg
 from pathlib import Path
 
-version = "0.3.1"
+VERSION = "0.3.1"
+TEMPLATES_DIR = "templates"
+IMAGES_DIR = "images"
+ICON_PATH = "src/ai.ico"
+AI_IMAGE_PATH = "src/ai.png"
+
 
 class Helper(ctk.CTk):
-
     def __init__(self, context):
-        f"""
-        이 코드는 "Hwp Helper v.{version}"라는 제목의 GUI 어플리케이션을 생성하는데 사용됩니다. 주로 tkinter, customtkinter, PIL, yaml, win32gui와 같은 여러 파이썬 라이브러리를 활용합니다. 이 코드는 주로 창을 만들고 설정하는데 중점을 두고 있습니다.
-
-        class Helper(ctk.CTk): Helper라는 이름의 클래스는 ctk.CTk 클래스를 상속하며, 이 클래스는 tkinter의 Tk 클래스를 상속받아서 제작된 것입니다. 따라서 이 클래스는 창을 만들고 제어하는 메서드들을 가지고 있습니다.
-        __init__(self, context): 이 메서드는 Helper 인스턴스를 초기화합니다. 여기서는 여러 가지 변수를 설정하고, 필요한 요소들을 화면에 배치합니다.
-        set_windows(self, left, top, width, height): 이 메서드는 창의 위치와 크기를 설정합니다.
-        set_fullscreen(self), set_halfscreen(self): 이 두 메서드는 창을 전체 화면으로 설정하거나, 화면의 절반 크기로 설정합니다.
-        check_hwp(self): 이 메서드는 한/글 애플리케이션이 실행 중인지 확인하고, 실행 중이라면 그 애플리케이션을 최상위 창으로 만드는 역할을 합니다.
-        set_window(self, x, y, width, height), get_window(self): 이 두 메서드는 창의 위치와 크기를 설정하거나 가져옵니다.
-        on_closing(self): 이 메서드는 창이 닫힐 때 실행되는 메서드로, 설정을 저장하고 창을 파괴하는 역할을 합니다.
-        """
         super().__init__()
-
-        self.version = f"Hwp Helper v.{version}"
         self.context = context
+        self._setup_basic_attributes()
+        self._create_essential_folders()
+        self._setup_ui()
+        self.app = context["app"]
 
+        
         # create essential folder
         Path("templates").mkdir(exist_ok=True)
         Path("images").mkdir(exist_ok=True)
-
+        
+        
         context["helper"] = self
 
-        # Override the default close behavior
+
+
+
+    def _setup_basic_attributes(self):
+        self.version = f"Hwp Helper v.{VERSION}"
+        self.context["helper"] = self
+        self.title(self.version)
+        self.iconbitmap(get_path(ICON_PATH))
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
 
-        self.title(self.version)
-        self.iconbitmap(get_path("src/ai.ico"))
+    def _create_essential_folders(self):
+        Path(TEMPLATES_DIR).mkdir(exist_ok=True)
+        Path(IMAGES_DIR).mkdir(exist_ok=True)
 
-        # set app
-        self.app = context["app"]
+    def _setup_ui(self):
+        self._setup_menu()
+        self._setup_tabview()
+        self._setup_frames()
+        self._setup_navigation_bar()
 
-        # set navi bar
-
+    def _setup_menu(self):
         self.menu = ctk.CTkFrame(self)
         self.menu.pack(fill=tk.X)
+        self._setup_menu_contents()
 
-        self.tabview = tabview = ctk.CTkTabview(master=self)
-        tabview.pack(padx=1, pady=1, fill="both", expand=True)
-        context["tabview"] = tabview
+    def _setup_menu_contents(self):
+        icon_image = ImageTk.PhotoImage(Image.open(get_path(AI_IMAGE_PATH)).resize((50, 50)))
+        icon = ctk.CTkLabel(self.menu, image=icon_image, text="")
+        icon.image = icon_image  # keep a reference
+        icon.pack(side=tk.LEFT, padx=5, pady=5)
 
-        tabview.add("templates")  # add tab at the end
-        tabview.add("features")  # add tab at the end
+        header = ctk.CTkLabel(self.menu, text=self.version)
+        header.pack(side=tk.LEFT, padx=5, pady=5)
 
-        tabname = context["setting"].get("tab", "features")
-        tabview.set(tabname)  # set currently visible tab
+    def _setup_tabview(self):
+        self.tabview = ctk.CTkTabview(master=self)
+        self.tabview.pack(padx=1, pady=1, fill="both", expand=True)
+        self.context["tabview"] = self.tabview
+        self.tabview.add("templates")
+        self.tabview.add("features")
+        self._set_initial_tab()
 
-        # set category frame
-        template_frame = CategoryFrame(tabview.tab("templates"), context)
+    def _set_initial_tab(self):
+        initial_tab = self.context["setting"].get("tab", "features")
+        self.tabview.set(initial_tab)
+
+    def _setup_frames(self):
+        self._setup_template_frame()
+        self._setup_feature_frame()
+
+    def _setup_template_frame(self):
+        template_frame = CategoryFrame(self.tabview.tab("templates"), self.context)
         template_frame.pack(fill="both", expand=True)
-        context["template_frame"] = template_frame
+        self.context["template_frame"] = template_frame
 
-        # set feature frame
-        feature_frame = HwpFeatureFrame(tabview.tab("features"), context)
+    def _setup_feature_frame(self):
+        feature_frame = HwpFeatureFrame(self.tabview.tab("features"), self.context)
         feature_frame.pack(fill="both", expand=True)
-        context["feature_frame"] = feature_frame
+        self.context["feature_frame"] = feature_frame
 
-        icon = ctk.CTkLabel(
-            self.menu,
-            text="",
-            image=ctk.CTkImage(Image.open(get_path("src/ai.png")), size=(50, 50)),
-            compound="left",
-        )
-        icon.pack(anchor="w", side=tk.LEFT, padx=5, pady=5)
-        header = ctk.CTkLabel(
-            self.menu,
-            text=self.version,
-        )
-        header.pack(anchor="w", side=tk.LEFT, padx=5, pady=5)
+    def _setup_navigation_bar(self):
+        navi_bar = NaviBar(self.menu, self.context)
+        navi_bar.pack(side=tk.RIGHT)
+        self.context["navi_bar"] = navi_bar
 
-        navi_bar = NaviBar(self.menu, context)
-        navi_bar.pack(anchor="e", side=tk.RIGHT)
-        context["navi_bar"] = navi_bar
+    # Rest of the methods (set_windows, set_fullscreen, etc.) remain largely unchanged
+
+    def on_closing(self):
+        current_tab = self.tabview.get()
+        self.context["setting"]["tab"] = current_tab
+        with open("setting.yaml", "w") as f:
+            yaml.safe_dump(self.context["setting"], f)
+        self.destroy()
 
     def set_windows(self, left, top, width, height):
         ratio = get_ratio(self)
@@ -155,16 +169,12 @@ class Helper(ctk.CTk):
         hwnd = wg.GetParent(self.winfo_id())
         return get_window_position(hwnd)
 
-    def on_closing(self):
-        current_tab = self.tabview.get()
-        self.context["setting"]["tab"] = current_tab
-        with open("setting.yaml", "w") as f:
-            yaml.safe_dump(self.context["setting"], f)
-        self.destroy()
 
 
 if __name__ == "__main__":
-    context = {"app": None}
+    from hwpapi.core import App
+    
+    context = {"app": App()}
     with open("setting.yaml", encoding="utf-8") as f:
         context["setting"] = yaml.safe_load(f)
     app = Helper(context)
